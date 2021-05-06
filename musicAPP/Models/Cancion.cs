e7179ed5e262458353bd613bf6befeefb093d4a8
xPashox <fco.alvarez.08@gmail.com>
2021-05-05 20:35:42 -0400

    Se agrega estructura MVC al directorio del proyecto

---
+++ musicAPP/Models/Cancion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace musicAPP
+{
+    class Cancion
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Autor { get; set; }
+        public String Album { get; set; }
+        public String Ubicacion { get; set; }
+
+    }
+}
