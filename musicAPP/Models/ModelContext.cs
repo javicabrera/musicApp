2d9a6b284cf1fee79e42af33136af16858805334
xPashox <fco.alvarez.08@gmail.com>
2021-05-07 12:28:03 -0400

    Se agrega el ModelContext

---
+++ musicAPP/Models/ModelContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace musicAPP.Models
+{
+    public class ModelContext : DbContext
+    {
+        public ModelContext() : base("name=local") { }
+        public DbSet<Cancion> Canciones { get; set; }
+        public DbSet<PlayList> Listas { get; set; }
+    }
+}
---
+++ musicAPP/Models/PlayList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace musicAPP.Models
+{
+    public class PlayList
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public List<Cancion> Lista { get; set; }
+    }
+}
